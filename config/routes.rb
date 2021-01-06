8038ab7aa80fe0a43b15d7fb58960e84d0752b75
defoebrand <defoe.brand@gmail.com>
2021-01-06 23:10:20 +0200

    Change delete route action to POST method

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'books#index'
 
   post '/new_book', to: 'books#create'
-  delete '/remove_book', to: 'books#destroy'
+  post '/remove_book', to: 'books#destroy'
 end
